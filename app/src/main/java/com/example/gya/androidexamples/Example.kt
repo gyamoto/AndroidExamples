1035fff1982ca2f4a03797a1f4591e50dd29c4b5
gyamoto <farundorlnightray@gmail.com>
2017-03-05 18:44:54 +0900

    fix description

--- app/src/main/java/com/example/gya/androidexamples/Example.kt
+++ app/src/main/java/com/example/gya/androidexamples/Example.kt
@@ -1,7 +1,7 @@
 package com.example.gya.androidexamples
 
 enum class Example(val description: String) {
-    BehaviorNotify("RecyclerViewのスクロールをBehaviorでうけとる動作確認"),
-    MultiRecycler("RecyclerViewを縦に2つ重ねての動作確認"),
-    FragmentShared("FragmentTransitionしたときのSharedElementTransition"),
+    BehaviorNotify("RecyclerViewのスクロールをBehaviorでうけとる"),
+    MultiRecycler("RecyclerViewを縦に2つ重ねてみる"),
+    FragmentShared("FragmentTransitionしたときViewをSharedElementでアニメーション"),
 }

a4c78eb4dd6fcc8da283105d16481a3a9945104f
gyamoto <farundorlnightray@gmail.com>
2017-07-02 19:24:39 +0900

    fix to calculate offset by column count

--- app/src/main/java/com/example/gya/androidexamples/griddecoration/GridItemDecoration.kt
+++ app/src/main/java/com/example/gya/androidexamples/griddecoration/GridItemDecoration.kt
@@ -16,30 +16,40 @@ class GridItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
     private val margin = context.resources.getDimensionPixelSize(R.dimen.grid_decoration_margin)
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        val manager = parent.layoutManager as? GridLayoutManager
         val params = view.layoutParams as? GridLayoutManager.LayoutParams
-        val isTop = parent.getChildAdapterPosition(view) == 0
-        val isStart = params?.spanIndex?.let { it == 0 } ?: false
-        val isEnd = params?.let { it.spanIndex + it.spanSize == manager?.spanCount } ?: false
+        val spanIndex = params?.spanIndex ?: 0
+        val spanSize = params?.spanSize ?: 1
+        val spanCount = (parent.layoutManager as? GridLayoutManager)?.spanCount ?: 1
+        val virtualColumns = spanCount / spanSize
 
-        when (parent.findContainingViewHolder(view)) {
-            is LargeViewHolder -> {
-                outRect.set(margin, if (isTop) margin else 0, margin, margin)
-            }
-            is MediumViewHolder -> {
-                outRect.set(
-                        if (isStart) margin else margin / 2,
-                        if (isTop) margin else 0,
-                        if (isEnd) margin else margin / 2,
-                        margin)
-            }
-            is SmallViewHolder -> {
-                outRect.set(
-                        if (isStart) margin else if (isEnd) margin / 3 else margin * 2 / 3,
-                        if (isTop) margin else 0,
-                        if (isStart) margin / 3 else if (isEnd) margin else margin * 2 / 3,
-                        margin)
-            }
+        val isTop = parent.getChildAdapterPosition(view) == 0
+        val isStart = spanIndex == 0
+        val isEnd = spanIndex + spanSize == spanCount
+
+        outRect.set(
+                calculateOffsetStart(isStart, isEnd, virtualColumns),
+                calculateOffsetTop(isTop),
+                calculateOffsetEnd(isStart, isEnd, virtualColumns),
+                margin)
+    }
+
+    private fun calculateOffsetTop(isTop: Boolean): Int {
+        return if (isTop) margin else 0
+    }
+
+    private fun calculateOffsetStart(isStart: Boolean, isEnd: Boolean, columns: Int): Int {
+        return when {
+            isStart -> margin
+            isEnd -> margin / columns
+            else -> margin * (columns - 1) / columns
+        }
+    }
+
+    private fun calculateOffsetEnd(isStart: Boolean, isEnd: Boolean, columns: Int): Int {
+        return when {
+            isStart -> margin / columns
+            isEnd -> margin
+            else -> margin * (columns - 1) / columns
         }
     }
 }

eaa5da6ba00c8e8c0910de21500208a3ddcfa74c
gya <farundorlnightray@gmail.com>
2017-07-23 00:01:10 +0900

    add MultiRecycler (#12)

--- app/src/main/java/com/example/gya/androidexamples/Example.kt
+++ app/src/main/java/com/example/gya/androidexamples/Example.kt
@@ -1,12 +1,13 @@
 package com.example.gya.androidexamples
 
 import android.content.Context
-import com.example.gya.androidexamples.griddecoration.GridDecorationActivity
 import com.example.gya.androidexamples.behaviornotify.BehaviorNotifyActivity
+import com.example.gya.androidexamples.griddecoration.GridDecorationActivity
+import com.example.gya.androidexamples.multirecycler.MultiRecyclerActivity
 
 enum class Example(val description: String, val action: (Context) -> Unit) {
     BehaviorNotify("RecyclerViewのスクロールをBehaviorでうけとる", { BehaviorNotifyActivity.start(it) }),
-    MultiRecycler("RecyclerViewを縦に2つ重ねてみる", { Unit }),
+    MultiRecycler("RecyclerViewを縦に2つ重ねてみる", { MultiRecyclerActivity.start(it) }),
     FragmentShared("FragmentTransitionしたときViewをSharedElementでアニメーション", { Unit }),
     GridDecoration("ViewHolder/SpanSizeごとにDecorationを切り替える", { GridDecorationActivity.start(it) }),
 }
---
+++ app/src/main/java/com/example/gya/androidexamples/multirecycler/CollapsibleRecyclerAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.gya.androidexamples.multirecycler
+
+import android.support.v7.util.DiffUtil
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import kotlin.properties.Delegates
+
+/**
+ * [CollapsibleViewHolder]を表示する[RecyclerView.Adapter]
+ */
+class CollapsibleRecyclerAdapter : RecyclerView.Adapter<CollapsibleViewHolder>() {
+
+    companion object {
+        private const val VIEW_TYPE = 1
+    }
+
+    var itemClickListener: OnCollapsibleClick = { Unit }
+
+    var colors: List<Int> by Delegates.observable(emptyList(), { property, old, new ->
+        DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+
+            override fun getOldListSize(): Int = old.size
+
+            override fun getNewListSize(): Int = new.size
+
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean
+                    = old[oldItemPosition] == new[newItemPosition]
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean
+                    = old[oldItemPosition] == new[newItemPosition]
+
+        }).dispatchUpdatesTo(this)
+    })
+
+    override fun getItemCount(): Int = colors.size
+
+    override fun getItemViewType(position: Int): Int = VIEW_TYPE
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollapsibleViewHolder
+            = CollapsibleViewHolder.inflate(parent)
+
+    override fun onBindViewHolder(holder: CollapsibleViewHolder, position: Int) {
+        holder.color = colors[position]
+        holder.itemView.setOnClickListener { itemClickListener.invoke() }
+    }
+}
+
+/**
+ * [CollapsibleViewHolder]をクリックしたときのCallback
+ */
+typealias OnCollapsibleClick = () -> Unit

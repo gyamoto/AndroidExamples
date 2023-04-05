f71ba05e6a35d8131a7dd3949169eef52bc8d9d9
gyamoto <farundorlnightray@gmail.com>
2023-04-05 19:04:07 +0900

    prepare automate image with ci

--- foo.sh
+++ foo.sh
@@ -1,4 +1,3 @@
-
 md5 foo.txt > new_foo.md5
 
 if [ "$(diff foo.md5 new_foo.md5)" ]
@@ -6,13 +5,13 @@ then
     echo "diff exist"
 else 
     echo "no diff exist"
-    exit 
+    exit
 fi
 
 echo "continue"
 dot -T png foo.txt -o foo.png
 mv new_foo.md5 foo.md5
 
-git add foo.*
 git config --global user.name 'foo-user'
-git commit -m 'update foo'
+git add foo.*
+git commit -m 'update foo.*'
